ae487a97cb55439e630a616d02431de81f09f4fc
Wekoslav Stefanovski <swekster@gmail.com>
2016-08-26 11:42:08 +0200

    Added VS project

---
+++ VSDemo/MainConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person weko = new Person();
+            weko.FirstName = "Wekoslav";
+            weko.LastName = "Stefanovski";
+
+            Console.WriteLine(weko);
+        }
+    }
+}
